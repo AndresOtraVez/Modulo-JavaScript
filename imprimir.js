8d95151d92b89ff1f84bcd17e4c469e6371ab504
AndresOtraVez <serranoandres35@gmail.com>
2021-04-15 18:35:39 -0500

    1er ClaseJS

---
+++ imprimir.js
@@ -0,0 +1,33 @@
+/**
+ * Ejercicio 2
+ * Imprimir "Hola Mundo" usando code runner.
+ * NodeJs
+ */
+
+console.log("HelloWord")
+
+/**
+ * Ejercicio 3
+ * Imprimir el tipo de dato de nuestras variables
+ * 1.- Tomar del ejercicio 1, una variable de cada tipo e imprimirla usando console.log
+ * 2.- Usar typeof para saber el tipo de dato
+ * 7 en total
+ */
+
+var name = "david"
+var age = 22
+var myBoolean = true
+var favoriteEat = null
+var lenguage = undefined
+var myNull = null
+var cars = ["BMW", "Mercedes", "Aston Martin"]
+var sport = "Soccer"
+
+console.log("Tipo de dato ===>", typeof name)
+console.log("Tipo de dato ===>", typeof age)
+console.log("Tipo de dato ===>", typeof myBoolean)
+console.log("Tipo de dato ===>", typeof favoriteEat)    
+console.log("Tipo de dato ===>", typeof lenguage)
+console.log("Tipo de dato ===>", typeof myNull)
+console.log("Tipo de dato ===>", typeof cars)
+console.log("Tipo de dato ===>", typeof sport)
---
+++ variables.js
@@ -0,0 +1,95 @@
+// ====  Tipos de dato  ====
+// Boolean => true o false
+var myBoolean = true
+// Undefined => undefined
+var myUndefined = undefined
+// String (agrupa un conjunto de caracteres)
+var mario = "delgado"
+// Null (especifica que una variable estará sin valor)
+var myNull = null
+// Number (Especifica un numero, enteros <> flotantes)
+var myIntNumber = 27
+var myFloatNumber = 1.20
+// Objeto (Los objetos contienen una estructura de datos que mantienen un contexto)
+var myObject = {
+  nombre: "Bryan",
+  age: 27,
+  location: undefined,
+  average: 8.90,
+  hobbies: ['Cinema', 'Reading', 'Cycling', {name: "Gaming"}]
+}
+// Arreglos (Son similares a los objetos pero, sus valores van apuntando a un indice)
+var myArray = [1, "Bryan", true, 90.4, null, undefined, {name: "Gaming"}]
+
+/**
+ * Ejercicio 1
+ * Crear 3 diferentes variables usando cada tipo de dato
+ * Ejemplo:
+ * 
+ * // 3 variables de tipo string
+ * var myName = "Bryan"
+ * var dogsName = "Rulfo"
+ * var catName = "Carin"
+ * 
+ * // 3 de tipo number
+ * ...
+ * 
+ * // 3 de tipo Boolean
+ * var esMayorDeEdad = true
+ * var puedeVotar = true
+ * var enfermo = false
+ * 
+ * // 3 de tipo Objeto
+ * ...
+ * 
+ * Tiempo 8min
+ */
+
+/** String */
+var name = "david"
+var secondName = "Andrés"
+var sport = "Soccer"
+
+/**Object */
+var myperfil ={
+    Firstname: "david",
+    age: 22,
+    location: location,
+    hobbies: ['Swimming', 'Readbooks', 'Cycling', 'eat']
+}
+
+/**Boolean */
+ 
+var myBoolean = true
+var myname = true
+var age = true
+var nickname = false
+
+// Undefined
+var gender = undefined
+var trademark = undefined
+var lenguage = undefined
+
+/**Number */
+var height = 1.70
+var age = 22
+var birthday = 11/06
+
+/**Null */
+
+var lastname = null
+var birthdat = null
+var favoriteEat = null
+
+/*Array */
+var cars = ["BMW", "Mercedes", "Aston Martin"]
+var phones = ["iPhone", "Samsung", "Motorola"]
+var music = ["Rock", "Hip-Hop", "Pop"]
+
+// Camel case: Empiezas una palabra y la juntas con Mayuscula para formar una frase/
+
+var paisLatinoamericano = "México"
+var ahorroAnual = "60000"
+
+// Snake case. Juntas_palabras_con_guiones_bajos
+var deuda_total = "20000"
