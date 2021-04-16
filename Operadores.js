9e73deb37c39a12160b85c801cdc93ecf413b48a
AndresOtraVez <serranoandres35@gmail.com>
2021-04-15 22:12:06 -0500

    Ejercicios1

---
+++ Operadores.js
@@ -0,0 +1,70 @@
+var a = 5;
+var b = 10;
+var c = 15;
+// Comentarios sencillos (1 linea) 
+/**
+ * Comentarios multilinea
+ * Ejemplo
+ * 
+ */
+
+// // Multiplicación
+// a * b // 50
+// console.log(a * b)
+
+// // Adición
+// a + c // 20
+// console.log(a + c)
+
+// // División
+// b / a // 2
+// console.log(b / a)
+
+// // Resta
+// c - a // 10
+// // console.log(c - a)
+
+// // Modulo (Arroja el residuo de una división)
+// b % a // 0
+// // console.log(b % a)
+
+
+// Incremento o decremento.
+c++; // 16
+console.log(c) // 16
+c++;
+console.log(c) // 17
+
+b-- // 9
+console.log(b) // 9
+
+
+/**
+ * Ejercicio 1
+ * 1.- Declarar 4 variables
+ * 2.- Hacer 2 operaciones con cada uno de los operadores antes vistos
+ * 
+ */
+
+var a = 40;
+var b = 35;
+var c = 10;
+var d = 50;
+
+//multiplicación
+console.log(a * c)
+console.log(b * d)
+
+//Adición 
+console.log(a + b)
+console.log(d + a)
+
+//Resta
+console.log(b - c)
+console.log(d - a)
+
+//División
+console.log(b / c)
+console.log(c / b)
+
+// 
---
+++ controlesDeFlujo.js
@@ -0,0 +1,199 @@
+// Controles de flujo
+
+// Hardcode. Es agregar valores directamente al codigo.
+
+
+var a = 10
+var b = 50
+
+// console.log('a > b ==>', a > b)
+
+// if (a > b) {
+//   console.log("El número es mayor")
+// } else {
+//   console.log("El número es menor")
+// }
+
+/**
+ * If lo que evalua es un boleano (true o false)
+ * Si es true, ejecuta el primer bloque
+ * {
+    console.log("El número es mayor")
+  }
+ * 
+  si es falso entonces ejecuta el bloque del "else"
+  else {
+    console.log("El número es menor")
+  }
+ */
+
+// Valores truthys o falseys
+
+// Truthy
+// var myNumber = -10
+// var myNumber = 20
+// var myNumber = "Hola Mundo"
+// var myBolean = true
+// var myObject = {nombre: "Bryan"}
+// var myObject = {}
+// var myArray = [1,2,3]
+// var myArray = []
+
+
+// Falsey 
+// var myNumber = 0
+// var myString = ""
+// var myBolean = false
+// var myUndefined = undefined
+// var myNull = null
+// var myNan = NaN
+
+// var myNumber = -10
+
+// if (myNumber) {
+//   console.log("Truthy")
+// } else {
+//   console.log("Falsey")
+// }
+
+
+
+// if (3 !== "3") {
+//   console.log('3 == "3" es diferente')
+// } else {
+//   console.log('3 == "3" no es diferente')
+// }
+
+// if (3 === "3") {
+//   console.log('3 === "3" es truthy')
+// } else {
+//   console.log('3 === "3" es falsey')
+// }
+
+
+
+
+
+// if (10 < 10) {
+//   console.log('10 < 10 es verdadero')
+// } else {
+//   console.log('10 < 10 es falso')
+// }
+
+/**
+ * Ejercicio 2
+ * 1 .- Vamos a crear condiciones usando if y utilizando cada uno de los operadores logicos (8)
+ * 2.- Crear condiciones con 3 truthys y 3 falseys (6)
+ * Tiempo 15 minutos.
+ */
+
+
+var a = 20
+var b = 30
+var c = 5
+var d = 5
+
+//Igualdad
+if (a == b) {
+    console.log("El número es igual")
+ } else {
+      console.log("El número es diferente")
+    }
+
+//Diferente 
+if (a != b) {
+    console.log("El número es igual")
+ } else {
+      console.log("El número es diferente")
+    }
+
+//EstrictamenteIguales
+if (9 === "9") {
+    console.log('3 === "3" es truthy')
+} else {
+    console.log('3 === "3" es falsey')
+}
+
+//EstrictamenteDesiguales 
+if (9 !== "9") {
+    console.log('9 === "9" es truthy')
+} else {
+    console.log('9 === "9" es falsey')
+}
+
+//MayorQue
+if (a > b) {
+    console.log('5 > 4 Verdadero')
+} else {
+    console.log('5 > 4 Falso')
+}
+
+//MenorQue 
+if (a < b) {
+    console.log('5 < 4 Verdadero')
+} else {
+    console.log('5 < 4 Falso')
+}
+
+//MayorOIgualQue
+if (c >= d) {
+    console.log('c >= d Verdadero')
+} else {
+    console.log('c >= d Falso')
+}
+
+//MenorOIgualQue
+if (c <= d) {
+    console.log('c <= d Verdadero')
+} else {
+    console.log('c <= d Falso')
+}
+
+
+/*switch (mes) {
+    case "enero":
+        console.log("El mes es enero")
+      break
+      case "febrero":
+        console.log("El mes es febrero")
+      break
+      default: 
+        console.log("El mes no fue encontrado")
+    */
+
+    
+    
+    /**
+     * Ejercicio 3
+     * 1.- Hacer un switch que responde el color de ropa
+     * 2.- Enviar un color
+     * 3.- Los cases tendrán diferentes colores. Al menos 6 colores distintos
+     * 4.- Van a imprimir de que color es la ropa
+     * 10 minutos
+     */
+
+
+var color = 'rojo'
+
+switch (color) {
+    case 'azul':
+        console.log('colorazul')
+        break
+    case 'verde':
+        console.log('colorverder')
+        break
+    case 'amarillo':
+        console.log('coloramarillo')
+        break
+    case 'morado':
+        console.log('colormorado')
+        break
+    case 'cafe':
+        console.log('colorcafe')
+        break
+    case 'rojo':
+        console.log('colorrojo')
+        break
+    default:
+        console.log('color no fue encontrado')
+}
